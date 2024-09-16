ace431e4179565920cdf4618cfa4ae1420da99d7
Raphael-08 <mohanvarma130@gmail.com>
2024-09-16 16:34:51 +0530

    fix: made t3-oss working

--- src/env.ts
+++ src/env.ts
@@ -3,13 +3,17 @@ import { z } from "zod";
 
 export const env = createEnv({
   client: {
-    NEXT_PUBLIC_DEV_URL: z.string().optional(),
-    NEXT_PUBLIC_BASE_URL: z.string().optional(),
-    NEXT_PUBLIC_NODE_ENV: z.string().optional(),
+    NEXT_PUBLIC_DEV_URL: z.string().min(1),
+    NEXT_PUBLIC_BASE_URL: z.string().min(1),
+    NEXT_PUBLIC_NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
+    NEXT_PUBLIC_GOOGLE_API_KEY: z.string().min(1),
   },
   experimental__runtimeEnv: {
-    NEXT_PUBLIC_DEV_URL: process.env.DEV_APP_URL,
-    NEXT_PUBLIC_BASE_URL: process.env.APP_URL,
-    NEXT_PUBLIC_NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_DEV_URL: process.env.NEXT_PUBLIC_DEV_APP_URL,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV,
+    NEXT_PUBLIC_GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
   },
 });
